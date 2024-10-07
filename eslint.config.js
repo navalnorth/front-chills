9ed36bbefd69cc018bcdb78f1f96094337d5ff5e
navalnorth <moha_naval@live.com>
2024-10-07 11:29:37 +0200

    comopsant

--- eslint.config.js
+++
@@ -1,9 +0,0 @@
-import globals from "globals";
-import pluginVue from "eslint-plugin-vue";
-
-
-export default [
-  {files: ["**/*.{js,mjs,cjs,vue}"]},
-  {languageOptions: { globals: globals.browser }},
-  ...pluginVue.configs["flat/essential"],
-];
