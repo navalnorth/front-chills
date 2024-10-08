396f6b1d2ed7174224af1af50843a707d15cc87b
tOCHSKa <144660217+tOCHSKa@users.noreply.github.com>
2024-10-08 09:18:44 +0200

    ajout img

--- components/footer.vue
+++ components/footer.vue
@@ -1,3 +1,37 @@
 <template>
-
+    <div>
+        <div>logo</div>
+        <div>button</div>
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div>
+            <p><span></span></p>
+        </div>
+        <!-- <div>
+           <div v-for="(link, index) in linkArray" :key="index">
+            <a :href="link.url">{{ link.text }}</a>
+           </div>
+        </div>
+        <div>
+           <div v-for="(link, index) in linkArray" :key="index">
+            <a :href="link.url">{{ link.text }}</a>
+           </div>
+        </div>
+        <div>
+           <div v-for="(link, index) in linkArray" :key="index">
+            <a :href="link.url">{{ link.text }}</a>
+           </div>
+        </div> -->
+    </div>
 </template>
+
+
+<script setup>
+
+</script>
+<style lang="">
+    
+</style>
