53c087a7ac3e903eb647b306bc561a392c7fa919
tOCHSKa <144660217+tOCHSKa@users.noreply.github.com>
2024-10-14 21:25:08 +0200

    Banner & BannerFilm

---
+++ components/BannerFilm.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="grid-container">
+        <div class="item item1"><Card /></div>
+        <div class="item item2">
+            <div class="item2-title">
+                <p>TITRE DE FILM \/</p>
+            </div>
+            <div class="item2-info">
+                <p>Durée <span> | </span>Réalisateur</p>
+            </div>
+            <div class="item2-desc">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda eveniet distinctio commodi nobis ad maiores omnis explicabo mollitia! Odio, repellendus. Dolor, numquam eos accusamus ea nihil non nisi nulla consequatur adipisci dignissimos tempora, esse beatae magni animi illum sit eius dolorem? Incidunt odio sequi, magnam maxime placeat nihil quaerat similique.</p>
+            </div>
+        </div>
+        <div class="item item3">
+            <BoutonText class="bouton" textColor="var(--textcolorBlanc)" background="var(--colorbgNoir)" borderSolid="var(--borderRouge)">
+                BANDE ANNONCE
+            </BoutonText>
+        </div>
+        <div class="item item4">
+            <BoutonText class="bouton" textColor="var(--textcolorBlanc)" background="var(--colorbgRouge)" borderSolid="var(--borderRouge)">
+                VOIR LE FILM
+            </BoutonText>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import BoutonPlayVideo from './BoutonPlayVideo.vue';
+
+
+</script>
+
+<style scoped>
+.grid-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr; 
+    grid-template-rows: auto auto; 
+    gap: 10px; 
+    padding: 20px;
+    background-color: var(--colorbgNoir); 
+}
+
+.item {
+    color: white;
+    display: flex;
+    flex-direction: column;
+}
+
+.item1, .item2 {
+    height: 233px;
+    font-size: 10px;
+    font-family: var(--fontFamilyAndale);
+}
+
+.item1 {
+    display: flex;
+    align-items: center;
+}
+
+.item2-title, .item2-info {
+    text-align: left; /* Aligne le texte à gauche */
+}
+.item2-title {
+    font-size: 14px;
+}
+.item2-info {
+    margin-bottom: 20px;
+    font-size: 10px;
+}
+.item2-title p,
+.item2-info p {
+    margin: 0; /* Supprime les marges par défaut */
+}
+
+.item2-desc {
+    font-family: var(--fontFamilyMention);
+}
+
+.item3, .item4 {
+    min-height: 50px;
+
+    display: flex;
+    align-items: center;
+}
+
+.bouton {
+    font-size: 16px;
+    padding: 10px 4px;
+    width: 100%;
+}
+</style>
---
+++ components/Subtitle.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="subtitle" :style="backgroundStyle">
+        <p :style="pStyle">
+            {{ props.title }}
+        </p>
+    </div>
+</template>
+
+<script setup>
+
+import griffeTitre from '@/assets/img/griffeTitre.png';
+
+const props = defineProps({
+    title: {
+        type: String,
+        default: 'N/A'
+    },
+    showImage: {
+        type: Boolean,
+        default: false
+    },
+    centeredTitle: {
+        type: Boolean,
+        default: false
+    }
+
+});
+
+const backgroundStyle = computed(() => {
+    return {
+        backgroundImage: props.showImage ? `url(${griffeTitre})` : 'none',
+        backgroundSize: 'cover',
+        backgroundPosition: '-50px center',
+        backgroundRepeat: 'no-repeat',
+        height: '40px',
+        color: 'var(--textcolorBlanc)',
+        fontFamily: 'var(--fontFamily)',
+        paddingLeft: '17px',
+        position: 'relative',
+        overflow: 'visible',
+        display: 'flex',
+        justifyContent: props.centeredTitle ? 'center' : 'none',
+        backgroundColor: 'black',
+    };
+});
+
+</script>
+
+<style scoped>
+.subtitle {
+    display: flex;
+}
+.subtitle p {
+    margin: 0;
+    position: relative;
+    display: flex;
+    align-items: center;
+
+}
+</style>
